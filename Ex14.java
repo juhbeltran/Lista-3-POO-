47b42a1938ffb8fecd671a027e47afd2eb8899df
juhbeltran <91682581+juhbeltran@users.noreply.github.com>
2021-10-15 21:21:03 -0300

    Update Ex14.java

--- Ex14.java
+++ Ex14.java
@@ -5,12 +5,12 @@ public class Ex14 {
 	public static void main(String args[]) {
 
 		Scanner entrada = new Scanner(System.in);
-		int fatorial = 1;
+		
 		int s = 0;
 		int b = 0;
 		System.out.print("Entre com o numero: ");
 		int n1 = entrada.nextInt();
-
+                System.out.print("Série = ");
 		for (int i = 1; i <= n1; i++) {
 
 			s = s + 1;
